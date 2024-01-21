2393f7361ec9f664d679525d0ec0c12d69a375dc
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-22 00:14:46 +0800

    Pseudocode

---
+++ display_divisible_by_5.py
@@ -0,0 +1,9 @@
+# Exercise 6: Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only 
+# those numbers which are divisible by 5
+
+# Pseudocode
+# assign variable for list
+# for loop the list
+# if-else statement
+#   if num%, print list
